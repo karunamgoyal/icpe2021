a685c882f45ee488f3dc09ef028e6534994e7f70
Karunam Goyal <karunam.goyal25@gmail.com>
2020-09-20 15:38:24 +0530

    Updates

---
+++ precache-manifest.2c7a622babf991cdc252b1941ec7b51b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcea954fd0ee277c2115bd59da1af0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc6e0a7be41c77143950",
+    "url": "./static/css/main.60481ad5.chunk.css"
+  },
+  {
+    "revision": "ba260b5e67c044e28d3d",
+    "url": "./static/js/2.12bb2f69.chunk.js"
+  },
+  {
+    "revision": "39473544d74cced5be6d8895cef840b5",
+    "url": "./static/js/2.12bb2f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6e0a7be41c77143950",
+    "url": "./static/js/main.ccda596d.chunk.js"
+  },
+  {
+    "revision": "57b2c198af138b10dea3",
+    "url": "./static/js/runtime-main.fe266ba8.js"
+  },
+  {
+    "revision": "dcd6a76642e62b859ee3163ad5a55f8e",
+    "url": "./static/media/combined3.dcd6a766.png"
+  },
+  {
+    "revision": "4147f447d3324687383414b04603e8fa",
+    "url": "./static/media/image.4147f447.jpg"
+  },
+  {
+    "revision": "d186e37fe52508986ba66bbce136061d",
+    "url": "./static/media/image.d186e37f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/icpe2021/precache-manifest.779f2b0b0d8e45141baf6308fba0174d.js"
+  "./precache-manifest.2c7a622babf991cdc252b1941ec7b51b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/icpe2021/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
